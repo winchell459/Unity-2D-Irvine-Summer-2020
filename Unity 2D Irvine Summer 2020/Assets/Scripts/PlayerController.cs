a21e2709c81e5993a3cc86e967e2817c7ac7dc71
winchell459 <johncmorris@gmail.com>
2020-05-30 13:45:43 -0700

    ready

--- Unity 2D Irvine Summer 2020/Assets/Scripts/PlayerController.cs
+++ Unity 2D Irvine Summer 2020/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,14 @@ public class PlayerController : MonoBehaviour
     private Animator anim;
 
     public GroundCheck GC;
-    
+
+    private bool jumping;
+    public float JumpingSpeedFraction = 0.25f;
+
+    public GameObject Body;
+
+    private bool dead = false;
+    public bool die = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,25 +30,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rb.velocity.x != 0) anim.SetBool("Walking", true);
-        else anim.SetBool("Walking", false);
-
-        if(GC.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if (!die && !dead)
         {
-            anim.SetBool("Jumping", true);
+            if (rb.velocity.x != 0) anim.SetBool("Walking", true);
+            else anim.SetBool("Walking", false);
+
+            if (GC.isGrounded && Input.GetKeyDown(KeyCode.Space) && anim.GetCurrentAnimatorClipInfo(0)[0].clip.name != "Eggman_inAir")
+            {
+                anim.SetBool("Jumping", true);
+                jumping = true;
+            }
+        }else if (die)
+        {
+            anim.SetTrigger("Dead");
+            //Die();
         }
     }
 
     private void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
+        if(!die && !dead)
+        {
+            float h = Input.GetAxis("Horizontal");
 
-        //calc force to add to player for WalkSpeed
-        float F = ((WalkSpeed * h - rb.velocity.x) / Time.deltaTime) * rb.mass;  //((V - Vi)/time) * mass
-        rb.AddForce(new Vector2(F, 0));
+            if (jumping) h = h * JumpingSpeedFraction;
 
-        if (h > 0) transform.localScale = new Vector3(1, 1, 1);
-        else if (h < 0) transform.localScale = new Vector3(-1, 1, 1);
+            //calc force to add to player for WalkSpeed
+            float F = ((WalkSpeed * h - rb.velocity.x) / Time.deltaTime) * rb.mass;  //((V - Vi)/time) * mass
+            rb.AddForce(new Vector2(F, 0));
+
+            if (h > 0) transform.localScale = new Vector3(1, 1, 1);
+            else if (h < 0) transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
 
         
     }
@@ -52,6 +73,33 @@ public class PlayerController : MonoBehaviour
         {
             float F = ((JumpSpeed - rb.velocity.y) / Time.deltaTime) * rb.mass;
             rb.AddForce(new Vector2(0, F));
+            
         }
+        jumping = false;
+    }
+
+    public float DeathForce = 500;
+    public void Die()
+    {
+        die = false;
+        dead = true;
+        int childCount = Body.transform.childCount;
+        Destroy(GetComponent<Animator>());
+        for(int i = childCount - 1; i >= 0; i -= 1)
+        {
+            Transform child = Body.transform.GetChild(i);
+            child.parent = null;
+            child.gameObject.AddComponent<Rigidbody2D>();
+            child.GetComponent<PolygonCollider2D>().enabled = true;
+
+            Vector2 destructVector = (child.position - transform.position); //.normalized;
+            child.GetComponent<Rigidbody2D>().AddForce(destructVector * DeathForce);
+            //child.position = position;
+        }
+        Body.transform.parent = null;
+        Body.GetComponent<PolygonCollider2D>().enabled = true;
+        Body.AddComponent<Rigidbody2D>();
+        Destroy(gameObject);
+        
     }
 }
