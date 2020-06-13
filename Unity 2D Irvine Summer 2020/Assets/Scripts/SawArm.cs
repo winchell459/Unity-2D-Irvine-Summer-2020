dabcbdaa61af9b49eecd65235bcfcd639839b560
winchell459 <johncmorris@gmail.com>
2020-06-13 16:59:04 -0700

    Day 5 end

---
+++ Unity 2D Irvine Summer 2020/Assets/Scripts/SawArm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawArm : MonoBehaviour
+{
+    public float Speed = 1;
+
+    public enum SawModes
+    {
+        Idle,
+        Rotation,
+        Reciprocation
+    }
+    public SawModes SawMode;
+
+    private void Start()
+    {
+        GetComponent<Animator>().speed = Speed;
+        if (SawMode == SawModes.Reciprocation) GetComponent<Animator>().SetTrigger("Reciprocation");
+        else if(SawMode == SawModes.Rotation) GetComponent<Animator>().SetTrigger("Rotation");
+    }
+
+    
+}
