925d2b9539b40f4a10cd88f2f10ff5edc8a04e97
winchell459 <johncmorris@gmail.com>
2020-06-20 16:21:44 -0700

    end of day 6

---
+++ Unity 2D Irvine Summer 2020/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHandler : MonoBehaviour
+{
+    public PlayerController Player;
+    public Transform Spawnpoint;
+    public CameraController Camera;
+
+    public GameObject PlayerPrefab;
+    public Animator CheckpointSetUI; //UI panel that displays the checkpoint set message
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Player)
+        {
+            Player = Instantiate(PlayerPrefab, Spawnpoint.position, Quaternion.identity).GetComponent<PlayerController>();
+            Camera.Player = Player.transform;
+        }
+    }
+
+    public void SetCheckpoint(Transform checkpoint)
+    {
+        Spawnpoint = checkpoint;
+        if(CheckpointSetUI) CheckpointSetUI.SetTrigger("CheckpointSet");
+    }
+}
